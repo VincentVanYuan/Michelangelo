9d51f994e3174fce93b4173a7e6ae98b5ce13690
元赛 <vincent.van.yuan@gmail.com>
2016-06-12 15:33:48 +0800

    update

--- src/main/java/com/vincent/study/test/TestBeanInMain.java
+++ src/main/java/com/vincent/study/test/TestBeanInMain.java
@@ -4,6 +4,8 @@ import com.vincent.study.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -16,14 +18,29 @@ import org.springframework.core.io.ClassPathResource;
  */
 public class TestBeanInMain {
 
-    private static final Logger logger = LogManager.getLogger(TestEnumInMain.class);
+    private static final Logger logger = LogManager.getLogger(TestBeanInMain.class);
+
+    private static final ClassPathResource resource = new ClassPathResource("test/test-bean.xml");
 
     public static void main(String[] args) {
 
-        ClassPathResource resource = new ClassPathResource("test/test-bean.xml");
-        BeanFactory beanFactory = new XmlBeanFactory(resource);
-        User user = beanFactory.getBean("user", User.class);
+        User user = getUserByBeanFactory();
+        //User user = getUserByDefault();
+
         logger.info("\n" + user.getUserId() + "\n" + user.getUserName() + "\n" + user.getUserAvatar() + "\n" + user.getUserAge() + "\n" + user.getUserBirth());
 
     }
+
+    private static User getUserByBeanFactory() {
+        BeanFactory beanFactory = new XmlBeanFactory(resource);
+        return beanFactory.getBean("user", User.class);
+    }
+
+    private static User getUserByDefault() {
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(factory);
+        reader.loadBeanDefinitions(resource);
+        return factory.getBean("user", User.class);
+    }
+    
 }
