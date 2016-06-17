8958d7acd937fdf9df2b21cfa53bc2fc98791077
元赛 <vincent.van.yuan@gmail.com>
2016-06-17 16:34:41 +0800

    add TestLambdasInMain

---
+++ src/main/java/com/vincent/study/test/TestLambdasInMain.java
@@ -0,0 +1,21 @@
+package com.vincent.study.test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 测试 Lambdas
+ *
+ * @author Vincent
+ * @version 1.0
+ * @since 2016-6-17
+ */
+public class TestLambdasInMain {
+
+    private static final Logger logger = LogManager.getLogger(TestLambdasInMain.class);
+
+    public static void main(String[] args) {
+
+
+    }
+}
